1446a0abbb3d6ad2ae57a3cbfd7f5db5680f8b70
innovad <innovad@users.noreply.github.com>
2022-06-05 10:28:45 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, request
+from flask_session import Session
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-products = []
+Session['products'] = []
 
 @app.route('/')
 def form():
-    return render_template('index.html', products=products)
+    return render_template('index.html', products=Session['products'])
 
 
 @app.route('/', methods=['POST'])
 def data():
-    products.append(request.form['product'])
-    return render_template('index.html', products=products)
+    Session['products'].append(request.form['product'])
+    return render_template('index.html', products=Session['products'])
 
 
 if __name__ == '__main__':
